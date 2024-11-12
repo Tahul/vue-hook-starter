9a542a35f98e777986d0a3a2111618ea2fc8b2ae
Robin Kehl <robin.kehl@singular-it.de>
2024-11-12 18:29:55 +0100

    chore: add module to vite playground

--- playgrounds/vite/src/components/HelloWorld.vue
+++ playgrounds/vite/src/components/HelloWorld.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { useToggle } from 'vue-composable-starter'
+
+const [toggleable, toggle] = useToggle()
 
 defineProps<{ msg: string }>()
-
-const count = ref(0)
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">
-      count is {{ count }}
+    <button type="button" @click="toggle">
+      toggle is {{ toggleable }}
     </button>
     <p>
       Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      <code>components/HelloWorld.vue</code> to test HMR updates.
     </p>
   </div>
 
--- playgrounds/vite/vite.config.ts
+++ playgrounds/vite/vite.config.ts
@@ -1,7 +1,17 @@
+/// <reference types="vitest" />
+import { resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: 'vue-composable-starter',
+        replacement: resolve(__dirname, '../../src/index.ts'),
+      },
+    ],
+  },
 })
