276b829ebef15398ef8b0a89915c9e3c4c7315d1
Robin Kehl <robin.kehl@singular-it.de>
2024-11-25 16:31:28 +0100

    fix: remove @nuxt/kit import form plugin

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,17 +1,11 @@
 import type { App } from 'vue'
 import type { VueComposableStarterPluginOptions } from '../types'
-import { resolve } from 'node:path'
-import { addImportsDir } from '@nuxt/kit'
-import ToggleComponent from '../components/Toggle'
 
 export const VueComposableStarterPlugin = {
   install(app: App, _options?: VueComposableStarterPluginOptions) {
     // app.provide(EXAMPLE, options?.example)
 
-    // Add auto imports
-    addImportsDir(resolve('./runtime/composables'))
-
-    app.component('Toggle', ToggleComponent)
+    // app.component('Toggle', ToggleComponent)
   },
 }
 
